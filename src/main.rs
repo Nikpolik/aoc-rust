d581b8e14b090ace81ff6b8f08bd833ed4704fcd
Nikos Polykandriotis <n.polikandriotis@skroutz.gr>
2021-12-10 02:40:46 +0200

    Day 9 part 1

--- src/main.rs
+++ src/main.rs
@@ -2,87 +2,52 @@
 
 mod helpers;
 
-use itertools::{iproduct, Itertools};
+use std::cmp::Ordering::{self, Less};
+use std::{fmt::Debug, fs::File, io::BufRead, io::BufReader};
 
-use std::{
-    fs::File,
-    io::{BufRead, BufReader, Error},
-};
+const FILENAME: &str = "./inputs/day9/input.txt";
 
-const FILENAME: &str = "./inputs/day8/test.txt";
+type Point = (usize, usize);
 
-fn parse_line(line: Result<String, Error>) -> String {
-    let normal_string = line.unwrap();
-    let output = normal_string.split(" | ").skip(1).next().unwrap();
-    // Alocate string to return
-    String::from(output)
+// maybe point arrays where better? You can also destruture and patter match.
+fn get_adjacent(point: Point, length_x: usize, length_y: usize) -> Vec<Point> {
+    let (x, y) = point;
+
+    let mut adjacent: Vec<Point> = Vec::new();
+
+    if x > 0 {
+        adjacent.push((x - 1, y));
+    }
+
+    if y > 0 {
+        adjacent.push((x, y - 1));
+    }
+
+    if x < length_x - 1 {
+        adjacent.push((x + 1, y));
+    }
+
+    if y < length_y - 1 {
+        adjacent.push((x, y + 1));
+    }
+
+    adjacent
 }
 
-fn decode(chars: &Vec<char>) -> u32 {
-    if chars.len() == 7 {
-        return 8;
+fn print_2d<T: std::fmt::Display>(items: &Vec<Vec<T>>) {
+    for _ in items[0].iter() {
+        print!("--");
     }
-    if chars.len() == 4 {
-        return 7;
-    }
-    if chars.len() == 3 {
-        return 4;
-    }
-    if chars.len() == 2 {
-        return 1;
-    }
-
-    // for permuation in ALLOWED_CHARS {
-    //     let mapped_chars = chars
-    //         .iter()
-    //         .map(|c| {})
-    //         .collect()
-    //         .map(|mut chars: Vec<char>| {
-    //             chars.sort();
-    //             println!("{:?} mapped", chars);
-    //             match chars[..] {
-    //                 ['a', 'b', 'c', 'd', 'e', 'f', 'g'] => 8, // 8
-    //                 ['b', 'c', 'd', 'e', 'f'] => 5,           // 5
-    //                 ['a', 'c', 'd', 'f', 'g'] => 2,           // 2
-    //                 ['a', 'b', 'c', 'd', 'f'] => 3,           // 3
-    //                 ['a', 'b', 'd'] => 7,                     // 7
-    //                 ['a', 'b', 'c', 'd', 'e', 'f'] => 9,      // 9
-    //                 ['b', 'c', 'd', 'e', 'f', 'g'] => 6,      // 6
-    //                 ['a', 'b', 'c', 'd', 'e', 'g'] => 4,      // 4
-    //                 ['a', 'b'] => 0,                          // 1
-    //                 _ => panic!("Could not decode value"),
-    //             }
-    //         });
-    // }
-    panic!("");
-}
-
-fn solve(output: &String) -> u32 {
-    let chars: Vec<Vec<char>> = output
-        .split(" ")
-        .map(|digit| (*digit).chars().collect::<Vec<char>>())
-        .collect();
-
-    let total: u32 = chars
-        .iter()
-        .map(decode)
-        .collect::<Vec<u32>>()
-        .iter()
-        .rev()
-        .enumerate()
-        .map(|(power, digit)| 10_u32.pow(power as u32) * *digit)
-        .sum();
-
-    println!("{:?}", total);
-    total as u32
-}
-
-fn build_permutations() {
-    for (a, b) in iproduct!(
-        ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
-        ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-    ) {
-        println!("{} {}", a, b);
+    println!("");
+    for row in items.iter() {
+        for col in row.iter() {
+            print!("{}|", col);
+        }
+        println!("");
+        for _ in row.iter() {
+            print!("--");
+        }
+        println!("");
     }
 }
 
@@ -90,9 +55,35 @@ fn main() {
     let file = File::open(FILENAME).expect("Could not read file");
     let reader = BufReader::new(file);
 
-    let outputs: Vec<String> = reader.lines().map(parse_line).collect();
-    let total: u32 = outputs.iter().map(solve).sum();
+    let mut rows: Vec<Vec<u32>> = Vec::new();
+    for line in reader.lines() {
+        let numbers: Vec<u32> = line
+            .unwrap()
+            .chars()
+            .filter_map(|digit| digit.to_digit(10))
+            .collect();
 
-    println!("-------");
-    println!("{}", total);
+        rows.push(numbers);
+    }
+
+    print_2d(&rows);
+    let mut score = 0;
+
+    for (x, cols) in rows.iter().enumerate() {
+        println!("{}", x);
+        for (y, value) in cols.iter().enumerate() {
+            println!("{} {}", x, y);
+            let low_point = get_adjacent((x, y), rows.len(), cols.len())
+                .iter()
+                .all(|(x, y)| {
+                    let other_value = rows[*x][*y];
+                    other_value.cmp(value) == Ordering::Greater
+                });
+            if low_point {
+                score += value + 1;
+            }
+        }
+    }
+
+    println!("{}", score);
 }
