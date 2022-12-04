f9a6524b2468b2c1fd696b8155042e9c977f2320
nikpolik <n.a.polikandriotis@gmail.com>
2022-12-05 01:43:50 +0200

    2022 day 4

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@
 
 mod helpers;
 
+use anyhow::{Error, Result};
 use std::cmp::Ordering::{self, Less};
 use std::collections::HashMap;
+use std::num::ParseIntError;
 use std::slice::SliceIndex;
+use std::str::FromStr;
 use std::{fmt, fmt::Debug, fs::File, io::BufRead, io::BufReader};
 
 use itertools::Itertools;
@@ -13,44 +16,56 @@ use itertools::Itertools;
 use crate::helpers::print_2d;
 use crate::helpers::StringUtils;
 
-const FILENAME: &str = "./inputs/2022/day3/test.txt";
+const FILENAME: &str = "./inputs/2022/day4/input.txt";
 
-fn get_letter_priorit(letter: char) -> usize {
-    let a_capital = 65;
-    let a = 97;
-    let letter_code = letter as usize;
+#[derive(Debug)]
+struct Pair {
+    start: u8,
+    end: u8,
+}
 
-    if letter_code >= a {
-        return 1 + letter_code - a;
-    } else {
-        return 27 + letter_code - a_capital;
+impl Pair {
+    pub fn contains(&self, other: &Self) -> bool {
+        self.start <= other.start && other.end <= self.end
+    }
+
+    pub fn overlapp(&self, other: &Self) -> bool {
+        other.end <= self.end && other.end >= self.start
+            || other.start <= self.end && other.start >= self.start
+    }
+}
+
+impl FromStr for Pair {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (start, end) = s.split_once('-').unwrap();
+
+        let start_fromstr = start.parse::<u8>()?;
+        let end_fromstr = end.parse::<u8>()?;
+
+        Ok(Self {
+            start: start_fromstr,
+            end: end_fromstr,
+        })
     }
 }
 
 fn main() {
     let file = File::open(FILENAME).expect("Could not read file");
     let reader = BufReader::new(file);
-    let mut total_priority: u64 = 0;
-    for group in reader.lines().chunks(3).into_iter() {
-        let mut all_letters: [u8; 53] = [0; 53];
-        for line in group {
-            let mut current_letters: [bool; 53] = [false; 53];
-            line.unwrap().chars().for_each(|letter| {
-                let priority = get_letter_priorit(letter);
-                current_letters[priority] = true;
-            });
-            for i in 0..current_letters.len() {
-                if current_letters[i] {
-                    all_letters[i] += 1;
-                }
-            }
-        }
-
-        for i in 0..all_letters.len() {
-            if all_letters[i] == 3 {
-                total_priority += i as u64;
-            }
-        }
-    }
-    println!("{}", total_priority);
+    let groups = reader
+        .lines()
+        .map(|line| {
+            line.unwrap().split_once(",").and_then(|(first, second)| {
+                Some((
+                    first.parse::<Pair>().unwrap(),
+                    second.parse::<Pair>().unwrap(),
+                ))
+            })
+        })
+        .filter_map(|v| v)
+        .filter(|(first, second)| first.overlapp(&second) || second.overlapp(&first))
+        .count();
+    println!("{:?}", groups);
 }
