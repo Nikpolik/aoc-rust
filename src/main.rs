47762ad459abacdbea16101ed711454855b57662
nikpolik <n.a.polikandriotis@gmail.com>
2022-12-09 12:34:20 +0200

    2022 day 8

--- src/main.rs
+++ src/main.rs
@@ -1,123 +1,124 @@
-#![allow(dead_code)]
-#![allow(unused)]
-
 mod helpers;
 
-use anyhow::{Error, Result};
-use std::cmp::Ordering::{self, Less};
-use std::collections::HashMap;
-use std::collections::VecDeque;
-use std::num::ParseIntError;
-use std::slice::SliceIndex;
-use std::str::FromStr;
-use std::{fmt, fmt::Debug, fs::File, io::BufRead, io::BufReader};
+type InputType = Vec<Vec<i32>>;
+type SolutionType = Vec<Vec<bool>>;
 
-use itertools::Itertools;
-
-use crate::helpers::print_2d;
-use crate::helpers::StringUtils;
-
-const FILENAME: &str = "./inputs/2022/day5/input.txt";
+fn parse_input(input_str: &str) -> InputType {
+    input_str
+        .lines()
+        .map(|line| {
+            line.chars()
+                .filter_map(|c| c.to_digit(10))
+                .map(|d| d as i32)
+                .collect()
+        })
+        .collect()
+}
 
 #[derive(Debug)]
-struct Stacks {
-    pub stacks: Vec<VecDeque<char>>,
+enum Side {
+    LeftRight,
+    TopBottom,
+    RightLeft,
+    BottomTop,
 }
 
-impl Stacks {
-    pub fn new() -> Self {
-        Self { stacks: Vec::new() }
-    }
-
-    pub fn parse_line(&mut self, line: String) {
-        line.chars()
-            .chunks(4)
-            .into_iter()
-            .enumerate()
-            .for_each(|(count, stack)| {
-                let current_char = stack.skip(1).next().unwrap();
-                if current_char.is_digit(10) {
-                    return;
-                }
-
-                if (self.stacks.len() < count + 1) {
-                    self.stacks.push(VecDeque::new());
-                }
-
-                let current_stack = self.stacks.get_mut(count).unwrap();
-                if (current_char == ' ') {
-                    return;
-                }
-                current_stack.push_front(current_char);
-            });
-    }
-
-    pub fn pop_many(&mut self, from: usize, amount: &usize) -> VecDeque<char> {
-        let initial_stack = self.stacks.get_mut(from).unwrap();
-        let final_len = initial_stack.len() - amount;
-        let results = initial_stack.split_off(final_len);
-        println!("{:?}", results);
-        results
-    }
-
-    pub fn append_to(&mut self, to: usize, chars: &mut VecDeque<char>) {
-        let current_stack = self.stacks.get_mut(to).unwrap();
-        for character in chars.iter() {
-            current_stack.push_back(*character);
-        }
+fn get_range(side: &Side, start: usize, end: usize) -> Box<dyn Iterator<Item = usize>> {
+    match side {
+        Side::LeftRight | Side::TopBottom => Box::new(start..end),
+        Side::RightLeft | Side::BottomTop => Box::new((start..end).rev()),
     }
 }
 
-impl fmt::Display for Stacks {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for _ in self.stacks[0].iter() {
-            write!(f, "----");
+fn find_clearings(i: usize, j: usize, input: &InputType) -> i32 {
+    let mut total = 1;
+    let mut side_score = 0;
+    // left
+    for k in get_range(&Side::RightLeft, 0, j) {
+        side_score += 1;
+        if input[i][k] >= input[i][j] {
+            break;
         }
-        writeln!(f, "");
-        let max = self.stacks.iter().map(|s| s.len()).max().unwrap();
+    }
+    total = total * side_score;
 
-        for row in self.stacks.iter() {
-            for col in row.iter() {
-                write!(f, " {} |", col);
-            }
-            writeln!(f, "");
-            for _ in row.iter() {
-                write!(f, "----");
-            }
-            writeln!(f, "");
+    // right
+    side_score = 0;
+    for k in get_range(&Side::LeftRight, j + 1, input.len()) {
+        side_score += 1;
+        if input[i][k] >= input[i][j] {
+            break;
         }
+    }
+    total = total * side_score;
 
-        Ok(())
+    // top
+    side_score = 0;
+    for k in get_range(&Side::BottomTop, 0, i) {
+        side_score += 1;
+        if input[k][j] >= input[i][j] {
+            break;
+        }
+    }
+    total = total * side_score;
+
+    // bottom
+    side_score = 0;
+    for k in get_range(&Side::TopBottom, i + 1, input.len()) {
+        side_score += 1;
+        if input[k][j] >= input[i][j] {
+            break;
+        }
+    }
+    total = total * side_score;
+    total
+}
+
+fn check_side(side: Side, input: &InputType, visibility_status: &mut SolutionType) {
+    let range = get_range(&side, 0, input.len());
+
+    for i in range {
+        let mut largest_height = -1;
+        let inner = get_range(&side, 0, input.len());
+
+        for j in inner {
+            let (first, second) = match side {
+                Side::LeftRight | Side::RightLeft => (i, j),
+                Side::TopBottom | Side::BottomTop => (j, i),
+            };
+            let current_row_height = input[first][second];
+            if current_row_height > largest_height {
+                visibility_status[first][second] = true;
+                largest_height = current_row_height;
+            }
+        }
     }
 }
 
 fn main() {
-    let file = File::open(FILENAME).expect("Could not read file");
-    let mut lines = BufReader::new(file).lines();
-    let mut stacks = Stacks::new();
-    while let Some(Ok(line)) = lines.next() {
-        if line == "" {
-            break;
+    let input_str = include_str!("../inputs/2022/day8/input.txt");
+    let input = parse_input(input_str);
+    let mut visibility_status: SolutionType = input
+        .iter()
+        .map(|row| row.iter().map(|_| false).collect())
+        .collect();
+    // first lets do one side, left to right
+    // scan items row by row and left to right
+    check_side(Side::TopBottom, &input, &mut visibility_status);
+    check_side(Side::BottomTop, &input, &mut visibility_status);
+    check_side(Side::RightLeft, &input, &mut visibility_status);
+    check_side(Side::LeftRight, &input, &mut visibility_status);
+
+    let mut max = 0;
+    for i in 0..visibility_status.len() {
+        for j in 0..visibility_status.len() {
+            if visibility_status[i][j] {
+                let score = find_clearings(i, j, &input);
+                if score > max {
+                    max = score;
+                }
+            }
         }
-
-        stacks.parse_line(line);
     }
-
-    println!("{}", stacks);
-    while let Some(Ok(line)) = lines.next() {
-        let action: Vec<usize> = line
-            .split(" ")
-            .filter_map(|part| part.safe_parse::<usize>())
-            .collect();
-
-        let amount = action.get(0).unwrap();
-        let from = action.get(1).unwrap() - 1;
-        let to = action.get(2).unwrap() - 1;
-        let mut elements = stacks.pop_many(from, amount);
-        println!("");
-        println!("Action amount: {}, from: {}, to: {}", amount, from, to);
-        println!("");
-        stacks.append_to(to, &mut elements);
-        println!("{}", stacks);
-    }
+    println!("{}", max);
 }
